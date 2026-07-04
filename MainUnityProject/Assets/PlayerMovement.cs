662c742f9f4b8ffb05b192103c15f30cf9675c85
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-04 18:39:14 +0200

    Player Movement and Broken Camera Controls

--- MainUnityProject/Assets/PlayerMovement.cs
+++ MainUnityProject/Assets/PlayerMovement.cs
@@ -1,14 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
 
+    // Declare Movement Variables
+    public InputAction moveAction;
+    public float speed;
+    Rigidbody rb;
+    
+    
     void Start()
     {
+        // Enable Movement
+        moveAction.Enable();
+        rb = GetComponent<Rigidbody>();
         
+        // Set Movement Speed
+        speed = 10;
+       
     }
     void Update()
     {
+        // Calculate Player Movement
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        Vector3 newVelocity = rb.linearVelocity;
         
+        newVelocity.x = moveInput.x * speed; 
+        newVelocity.z = moveInput.y * speed;
+
+        rb.linearVelocity = newVelocity;
+
     }
 }
