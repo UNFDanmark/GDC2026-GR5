e4dcb2818d45b5616581feac4ba9074626a75154
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-07 09:52:41 +0200

    Padlock numbers work

--- MainUnityProject/Assets/Scripts/Interactables/PadlockPuzzle1.cs
+++ MainUnityProject/Assets/Scripts/Interactables/PadlockPuzzle1.cs
@@ -7,10 +7,12 @@ public class PadlockPuzzle1 : MonoBehaviour
     float txt1 = 0;
     float txt2 = 0;
     float txt3 = 0;
+    float txt4 = 0;
 
     public TextMeshProUGUI display1;
     public TextMeshProUGUI display2;
     public TextMeshProUGUI display3;
+    public TextMeshProUGUI display4;
     
     float check = 0.5f;
     bool haveSolved;
@@ -20,6 +22,7 @@ public class PadlockPuzzle1 : MonoBehaviour
         display1.text = txt1.ToString();
         display2.text = txt2.ToString();
         display3.text = txt3.ToString();
+        display4.text = txt4.ToString();
     }
 
     public void IncreaseTxt1()
@@ -43,12 +46,19 @@ public class PadlockPuzzle1 : MonoBehaviour
         txt3 = Mathf.Repeat(txt3, 10);
         display3.text = txt3.ToString();
     }
+    
+    public void IncreaseTxt4()
+    {
+        txt4 += 1;
+        txt4 = Mathf.Repeat(txt4, 10);
+        display4.text = txt4.ToString();
+    }
 
     void Update()
     {
         if (!haveSolved)
         {
-            if (txt1 == 4 && txt2 == 7 && txt3 == 2)
+            if (txt1 == 1 && txt2 == 7 && txt3 == 0 && txt4 == 3)
             {
                 check -= Time.deltaTime;
 
