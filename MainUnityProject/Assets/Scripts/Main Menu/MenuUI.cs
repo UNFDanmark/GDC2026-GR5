4c91ba76d8e43bb848970b703ae5f53aec46247a
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-09 21:38:12 +0200

    Finished Demo!!!
    SUCCESS!
    
    PLz kill me

--- MainUnityProject/Assets/Scripts/Main Menu/MenuUI.cs
+++ MainUnityProject/Assets/Scripts/Main Menu/MenuUI.cs
@@ -1,26 +1,16 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
-    public GameObject Black; // Screen 0
-    public GameObject Logo; // Screen 1
-    public GameObject Lore1; // Screen 2
-    public GameObject Lore2; // Screen 3
-    float speed;
-    float cooldownLeft;
-    int screen = 0;
-
-    void Update()
-    {
-        
-    }
-
     public void NewGame()
     {
         SceneManager.LoadScene("Manor Room 1");
     }
-    
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
     
 }
