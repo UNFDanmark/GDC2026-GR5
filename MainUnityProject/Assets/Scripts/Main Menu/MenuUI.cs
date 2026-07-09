b693b1879c825ca8b11b2e692f470f5831a24137
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-09 17:12:30 +0200

    b

--- MainUnityProject/Assets/Scripts/Main Menu/MenuUI.cs
+++ MainUnityProject/Assets/Scripts/Main Menu/MenuUI.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
-{
+{   
+    int 
     public void NewGame()
     {
         SceneManager.LoadScene("Manor Room 1");
     }
+    
+    
 }
